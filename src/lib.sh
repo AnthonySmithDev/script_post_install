84b13c726305af82708b81242a56a14694d1a2ec
Anthony Smith <anthonyasdeveloper@gmail.com>
2022-05-01 12:21:28 -0500

    ✨ Add helpers functions

--- src/lib.sh
+++ src/lib.sh
@@ -15,3 +15,52 @@ source src/terminal.sh
 source src/validation.sh
 source src/debug.sh
 source src/os.sh
+
+lib::install() {
+	if check::command_exists $2; then
+		echo "$1 is already installed"
+	else
+		if interaction::prompt_yes_no "Do you want to install $1" "yes"; then
+			echo "Installing!"
+			($3)
+		else
+			echo "Skipping!"
+		fi
+	fi
+}
+
+lib::setup() {
+	if check::command_exists $2; then
+		if interaction::prompt_yes_no "Do you want to setup $1" "yes"; then
+			echo "Installing!"
+			($3)
+  		echo "Setup Successfully!"
+		else
+			echo "Skipping!"
+		fi
+	fi
+}
+
+lib::tool() {
+	if check::command_exists $2; then
+		if interaction::prompt_yes_no "Do you want to install $1" "no"; then
+			echo "Installing!"
+			($3)
+		else
+			echo "Skipping!"
+		fi
+  else
+		echo "$2 command not found!"
+	fi
+}
+
+lib::pack() {
+	if interaction::prompt_yes_no "Do you want to install $1" "yes"; then
+		echo "Starting!"
+		($2)
+		echo "Finished!"
+	else
+		echo "Skipping!"
+	fi
+}
+
